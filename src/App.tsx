6265c966f7ce7ce2b7f4fc86a3f319137250d71e
justz-hoo <1067744962@qq.com>
2023-01-28 11:17:37 +0800

    Redux Toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import { Contact } from './pages/Contact';
-import { Home } from './pages/Home';
-import { Login } from './pages/Login';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Contact } from "./pages/Contact";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Provider } from "react-redux";
+import { store } from './store';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/contact' element={<Contact />}/>
-        </Routes>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </Provider>
     </div>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import { login, logout } from "../store";
+import { useDispatch } from "react-redux";
+
 export const Login = () => {
+  const [userName, handleUserName] = useState<string>("");
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1>This is Login Page</h1>
-      <input type='text'></input>
-      <button>submit</button>
+      <input
+        type="text"
+        onChange={(event) => {
+          handleUserName(event.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          dispatch(login({ username: userName }));
+        }}
+      >
+        Submit Login
+      </button>
+      <button>Logout</button>
     </div>
-  )
+  );
 };
