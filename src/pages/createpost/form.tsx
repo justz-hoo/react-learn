505127d97ba16e76ff05b5f5761806e70579777d
justz-hoo <1067744962@qq.com>
2023-01-29 14:47:45 +0800

    project second part

--- src/pages/createpost/form.tsx
+++ src/pages/createpost/form.tsx
@@ -1,6 +1,9 @@
-import * as yup from 'yup';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 interface postData {
   title: string;
@@ -8,26 +11,44 @@ interface postData {
 }
 
 export const CreateForm = () => {
+  const [user] = useAuthState(auth);
+
   const schema = yup.object().shape({
-    title: yup.string().required('You must add a titile'),
-    description: yup.string().required('You must add a description to the post'),
+    title: yup.string().required("You must add a titile"),
+    description: yup
+      .string()
+      .required("You must add a description to the post"),
   });
 
-  const {register, handleSubmit, formState:{errors} } = useForm<postData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<postData>({
     resolver: yupResolver(schema),
   });
 
-  const onCreatePost = (data: postData) => {
-    console.log(data);
-  }
+  const postsRef = collection(db, "posts");
+
+  const onCreatePost = async (data: postData) => {
+    await addDoc(postsRef, {
+      // key1
+      // title: data.title,
+      // description: data.description,
+      // key2
+      ...data,
+      username: user?.displayName,
+      userId: user?.uid,
+    })
+  };
 
   return (
     <form onSubmit={handleSubmit(onCreatePost)}>
-      <input placeholder='Title ...' {...register('title')}/>
-      <p style={{color:'darkred'}}>{errors.title?.message}</p>
-      <textarea placeholder='Description' {...register('description')}/>
-      <p style={{color:'darkred'}}>{errors.description?.message}</p>
-      <input type='submit'/>
+      <input placeholder="Title ..." {...register("title")} />
+      <p style={{ color: "darkred" }}>{errors.title?.message}</p>
+      <textarea placeholder="Description" {...register("description")} />
+      <p style={{ color: "darkred" }}>{errors.description?.message}</p>
+      <input type="submit" />
     </form>
   );
 };
